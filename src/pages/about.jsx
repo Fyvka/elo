aba617842b5fb822fd8020915ed616cfac3a53d2
Fyvka <mrfoxdani4@gmail.com>
2024-06-06 23:30:46 +0300

    first commit

---
+++ src/pages/about.jsx
@@ -0,0 +1,14 @@
+import povar2 from './povar2.jpeg';
+
+function About () {
+    return(
+        <div>
+            <h1>Our the best chef</h1>
+            <img src={povar2} alt=""/>
+            <h5> We are waiting for you to visit us! </h5>
+            
+        </div>
+    );
+}
+
+export { About }
---
+++ src/pages/contact.jsx
@@ -0,0 +1,13 @@
+function Contact () {
+    return(
+        <div>
+            <h1>Contact us!</h1>
+            <h5></h5>
+            <h5>phone: 8(960)007-92-56</h5>
+            <h5>Telegram: @mrFyvka</h5>
+            
+        </div>
+    );
+}
+
+export { Contact }
